2e4fe59aa247fe5db2da5221b34636df7414e472
alnino98 <abhyelnino13@gmail.com>
2022-10-20 15:32:07 +0700

    first commit

---
+++ application/models/Model_brands.php
@@ -0,0 +1,62 @@
+<?php 
+
+class Model_brands extends CI_Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/*get the active brands information*/
+	public function getActiveBrands()
+	{
+		$sql = "SELECT * FROM brands WHERE active = ?";
+		$query = $this->db->query($sql, array(1));
+		return $query->result_array();
+	}
+
+	public function getSelectOption(){
+		$sql = "SELECT *  FROM brands ORDER BY brands.nama_barang ASC";
+		$data=mysql_fetch_array($sql); 
+	}
+	/* get the brand data */
+	public function getBrandData($id = null)
+	{
+		if($id) {
+			$sql = "SELECT * FROM brands WHERE id_barang = ?";
+			$query = $this->db->query($sql, array($id));
+			return $query->row_array();
+		}
+
+		$sql = "SELECT * FROM brands";
+		$query = $this->db->query($sql);
+		return $query->result_array();
+	}
+
+	public function create($data)
+	{
+		if($data) {
+			$insert = $this->db->insert('brands', $data);
+			return ($insert == true) ? true : false;
+		}
+	}
+
+	public function update($data, $id)
+	{
+		if($data && $id) {
+			$this->db->where('id_barang', $id);
+			$update = $this->db->update('brands', $data);
+			return ($update == true) ? true : false;
+		}
+	}
+
+	public function remove($id)
+	{
+		if($id) {
+			$this->db->where('id', $id);
+			$delete = $this->db->delete('brands');
+			return ($delete == true) ? true : false;
+		}
+	}
+
+}
---
+++ application/views/category/index.php
@@ -0,0 +1,423 @@
+ <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      Ajukan Barang
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Category</li>
+    </ol>
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+      <div class="col-md-12 col-xs-12">
+
+        <div id="messages"></div>
+
+        <?php if($this->session->flashdata('success')): ?>
+          <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo $this->session->flashdata('success'); ?>
+          </div>
+        <?php elseif($this->session->flashdata('error')): ?>
+          <div class="alert alert-error alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo $this->session->flashdata('error'); ?>
+          </div>
+        <?php endif; ?>
+
+        <?php if(in_array('createCategory', $user_permission)): ?>
+          <button class="btn btn-primary" data-toggle="modal" data-target="#addModal">Ajukan Barang</button>
+          <br /> <br />
+        <?php endif; ?>
+
+        <div class="box">
+          
+          <!-- /.box-header -->
+          <div class="box-body">
+            <table id="manageTable" class="table table-bordered table-striped">
+              <thead>
+              <tr>
+                <th>Nama Barang</th>
+                <th>Jumlah Barang</th>
+                <th>Departemen</th>
+                <th>Tanggal Pengajuan</th>
+                <th>Status</th>
+                <?php if(in_array('updateCategory', $user_permission) || in_array('deleteCategory', $user_permission)): ?>
+                  <th>Action</th>
+                <?php endif; ?>
+              </tr>
+              </thead>
+
+            </table>
+          </div>
+          <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+      </div>
+      <!-- col-md-12 -->
+    </div>
+    <!-- /.row -->
+    
+
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php if(in_array('createCategory', $user_permission)): ?>
+<!-- create brand modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="addModal">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Add Barang</h4>
+      </div>
+
+      <form role="form" action="<?php echo base_url('Controller_Category/create') ?>" method="post" id="createForm">
+
+        <div class="modal-body">
+
+        <!-- <div class="form-group">
+        <select class="form-control" id="nama_barang" name="nama_barang">
+            <option disable selected>~~SELECT~~</option>
+          <?php 
+            $data=mysql_fetch_array($sql); 
+            $sql=mysql_query("SELECT * FROM brands");
+            while ($data=mysql_fetch_array($sql)) {
+            ?>
+            <option value="<?=$data['brands']?>"><?=$data['brands']?></option> 
+            <?php
+              }
+             ?>
+        </select>
+         </div> -->
+         <div class="form-group">
+            <label for="nama_barang">Nama Barang</label>
+            <input type="text" class="form-control" id="nama_barang" name="nama_barang" placeholder="Enter category name" autocomplete="off">
+          </div>
+          <div class="form-group">
+            <label for="jml_barang">Jumlah Barang</label>
+            <input type="text" class="form-control" id="jml_barang" name="jml_barang" placeholder="Enter category name" autocomplete="off">
+          </div>
+          <div class="form-group">
+            <label for="departemen">Departemen</label>
+            <input type="text" class="form-control" id="departemen" name="departemen" placeholder="Enter category name" autocomplete="off">
+          </div>
+          <div class="form-group">
+            <label for="tgl_pengajuan">Tanggal Pengajuan</label>
+            <input type="date" class="form-control" id="tgl_pengajuan" name="tgl_pengajuan" placeholder="Enter category name" autocomplete="off">
+          </div>
+          <div class="form-group">
+            <label for="active">Status</label>
+            <select class="form-control" id="active" name="active">
+              <option value="1">Active</option>
+              <option value="2">Inactive</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save changes</button>
+        </div>
+      </form>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<?php endif; ?>
+
+<?php if(in_array('updateCategory', $user_permission)): ?>
+<!-- edit brand modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="editModal">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Edit Category</h4>
+      </div>
+
+      <form role="form" action="<?php echo base_url('Controller_Category/update') ?>" method="post" id="updateForm">
+
+        <div class="modal-body">
+          <div id="messages"></div>
+
+          <div class="form-group">
+            <label for="edit_nama_barang">Nama Barang</label>
+            <input type="text" class="form-control" id="edit_nama_barang" name="edit_nama_barang" placeholder="Enter nama barang" autocomplete="off">
+          </div>
+
+          <div class="form-group">
+            <label for="edit_jml_barang">Jumlah Barang</label>
+            <input type="text" class="form-control" id="edit_jml_barang" name="edit_jml_barang" placeholder="Enter jumlah barang" autocomplete="off">
+          </div>
+
+          <div class="form-group">
+            <label for="edit_departemen">Departemen</label>
+            <input type="text" class="form-control" id="edit_departemen" name="edit_departemen" placeholder="Enter departemen" autocomplete="off">
+          </div>
+
+          <div class="form-group">
+            <label for="edit_tgl_pengajuan">Tanggal Pengajuan</label>
+            <input type="text" class="form-control" id="edit_tgl_pengajuan" name="edit_tgl_pengajuan" placeholder="Enter tanggal pengajuan" autocomplete="off">
+          </div>
+
+          <div class="form-group">
+            <label for="edit_active">Status</label>
+            <select class="form-control" id="edit_active" name="edit_active">
+              <option value="1">Active</option>
+              <option value="2">Inactive</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save changes</button>
+        </div>
+
+      </form>
+
+
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<?php endif; ?>
+
+<?php if(in_array('deleteCategory', $user_permission)): ?>
+<!-- remove brand modal -->
+<div class="modal fade " tabindex="-1" role="dialog" id="removeModal">
+  <div class="modal-dialog modal-sm" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Remove Category</h4>
+      </div>
+
+      <form role="form" action="<?php echo base_url('Controller_Category/remove') ?>" method="post" id="removeForm">
+        <div class="modal-body">
+          <p>Do you really want to remove?</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-danger">Delete</button>
+        </div>
+      </form>
+
+
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<?php endif; ?>
+
+
+<script type="text/javascript">
+var manageTable;
+
+$(document).ready(function() {
+  $("#categoryNav").addClass('active');
+  
+  // initialize the datatable 
+  manageTable = $('#manageTable').DataTable({
+    dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'print'
+        ], 
+    'ajax': 'fetchCategoryData',
+    'order': []
+  });
+
+  // submit the create from 
+  $("#createForm").unbind('submit').on('submit', function() {
+    var form = $(this);
+
+    // remove the text-danger
+    $(".text-danger").remove();
+
+    $.ajax({
+      url: form.attr('action'),
+      type: form.attr('method'),
+      data: form.serialize(), // /converting the form data into array and sending it to server
+      dataType: 'json',
+      success:function(response) {
+
+        manageTable.ajax.reload(null, false); 
+
+        if(response.success === true) {
+          $("#messages").html('<div class="alert alert-success alert-dismissible" role="alert">'+
+            '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+            '<strong> <span class="glyphicon glyphicon-ok-sign"></span> </strong>'+response.messages+
+          '</div>');
+
+
+          // hide the modal
+          $("#addModal").modal('hide');
+
+          // reset the form
+          $("#createForm")[0].reset();
+          $("#createForm .form-group").removeClass('has-error').removeClass('has-success');
+
+        } else {
+
+          if(response.messages instanceof Object) {
+            $.each(response.messages, function(index, value) {
+              var id = $("#"+index);
+
+              id.closest('.form-group')
+              .removeClass('has-error')
+              .removeClass('has-success')
+              .addClass(value.length > 0 ? 'has-error' : 'has-success');
+              
+              id.after(value);
+
+            });
+          } else {
+            $("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">'+
+              '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+              '<strong> <span class="glyphicon glyphicon-exclamation-sign"></span> </strong>'+response.messages+
+            '</div>');
+          }
+        }
+      }
+    }); 
+
+    return false;
+  });
+
+});
+
+// edit function
+function editFunc(id)
+{ 
+  $.ajax({
+    url: 'fetchCategoryDataById/'+id,
+    type: 'post',
+    dataType: 'json',
+    success:function(response) {
+
+      $("#edit_category_name").val(response.name);
+      $("#edit_active").val(response.active);
+
+      // submit the edit from 
+      $("#updateForm").unbind('submit').bind('submit', function() {
+        var form = $(this);
+
+        // remove the text-danger
+        $(".text-danger").remove();
+
+        $.ajax({
+          url: form.attr('action') + '/' + id,
+          type: form.attr('method'),
+          data: form.serialize(), // /converting the form data into array and sending it to server
+          dataType: 'json',
+          success:function(response) {
+
+            manageTable.ajax.reload(null, false); 
+
+            if(response.success === true) {
+              $("#messages").html('<div class="alert alert-success alert-dismissible" role="alert">'+
+                '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+                '<strong> <span class="glyphicon glyphicon-ok-sign"></span> </strong>'+response.messages+
+              '</div>');
+
+
+              // hide the modal
+              $("#editModal").modal('hide');
+              // reset the form 
+              $("#updateForm .form-group").removeClass('has-error').removeClass('has-success');
+
+            } else {
+
+              if(response.messages instanceof Object) {
+                $.each(response.messages, function(index, value) {
+                  var id = $("#"+index);
+
+                  id.closest('.form-group')
+                  .removeClass('has-error')
+                  .removeClass('has-success')
+                  .addClass(value.length > 0 ? 'has-error' : 'has-success');
+                  
+                  id.after(value);
+
+                });
+              } else {
+                $("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">'+
+                  '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+                  '<strong> <span class="glyphicon glyphicon-exclamation-sign"></span> </strong>'+response.messages+
+                '</div>');
+              }
+            }
+          }
+        }); 
+
+        return false;
+      });
+
+    }
+  });
+}
+
+// remove functions 
+function removeFunc(id)
+{
+  if(id) {
+    $("#removeForm").on('submit', function() {
+
+      var form = $(this);
+
+      // remove the text-danger
+      $(".text-danger").remove();
+
+      $.ajax({
+        url: form.attr('action'),
+        type: form.attr('method'),
+        data: { category_id:id }, 
+        dataType: 'json',
+        success:function(response) {
+
+          manageTable.ajax.reload(null, false); 
+
+          if(response.success === true) {
+            $("#messages").html('<div class="alert alert-success alert-dismissible" role="alert">'+
+              '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+              '<strong> <span class="glyphicon glyphicon-ok-sign"></span> </strong>'+response.messages+
+            '</div>');
+
+            // hide the modal
+            $("#removeModal").modal('hide');
+
+          } else {
+
+            $("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">'+
+              '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+              '<strong> <span class="glyphicon glyphicon-exclamation-sign"></span> </strong>'+response.messages+
+            '</div>'); 
+          }
+        }
+      }); 
+
+      return false;
+    });
+  }
+}
+
+
+</script>
+<script type="text/javascript" src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
